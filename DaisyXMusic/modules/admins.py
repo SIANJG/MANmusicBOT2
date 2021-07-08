a340295af1c955727e7c35a9a7d404f7a12c5399
SIANJG <86875523+SIANJG@users.noreply.github.com>
2021-07-08 21:46:58 +0700

    Update admins.py

--- DaisyXMusic/modules/admins.py
+++ DaisyXMusic/modules/admins.py
@@ -1,34 +1,17 @@
 # Calls Music 1 - Telegram bot for streaming audio in group calls
 # Copyright (C) 2021  Roj Serbest
 
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as
-# published by the Free Software Foundation, either version 3 of the
-# License, or (at your option) any later version.
-
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
-#
-# You should have received a copy of the GNU Affero General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
-
-from asyncio import QueueEmpty
-from pyrogram import Client 
-from pyrogram import filters
-from pyrogram.types import Message
-
+from asyncio.queues import QueueEmpty
 from DaisyXMusic.config import que
+from pyrogram import Client, filters
+from pyrogram.types import Message
+import sira
+import tgcalls
 from DaisyXMusic.function.admins import set
 from DaisyXMusic.helpers.channelmusic import get_chat_id
-from DaisyXMusic.helpers.decorators import authorized_users_only
-from DaisyXMusic.helpers.decorators import errors
-from DaisyXMusic.helpers.filters import command
-from DaisyXMusic.helpers.filters import other_filters
+from DaisyXMusic.helpers.decorators import authorized_users_only, errors
+from DaisyXMusic.helpers.filters import command, other_filters
 from DaisyXMusic.services.callsmusic import callsmusic
-from DaisyXMusic.services.queues import queues
 
 
 @Client.on_message(filters.command("adminreset"))
@@ -36,57 +19,44 @@ async def update_admin(client, message: Message):
     chat_id = get_chat_id(message.chat)
     set(
         chat_id,
-        [
+        (
             member.user
-            for member in await message.chat.get_members(filter="administrators")
-        ],
+            for member in await message.chat.get_members(
+                filter="administrators"
+            )
+        ),
     )
-    await message.reply_text("<i>✅Admin cache refreshed!</i>!")
+
+    await message.reply_text("✅️ Admin cache refreshed!")
 
 
-@Client.on_message(command("pause") & other_filters)
+@Client.on_message(command(["pause", "jeda"]) & other_filters)
 @errors
 @authorized_users_only
 async def pause(_, message: Message):
-    chat_id = get_chat_id(message.chat)
-    if (chat_id not in callsmusic.active_chats) or (
-        callsmusic.active_chats[chat_id] == "paused"
-    ):
-        await message.reply_text("❗ Tidak ada yg diputar!")
-    else:
-        callsmusic.pause(chat_id)
-        await message.reply_text("▶️ Paused!")
+    callsmusic.pytgcalls.pause_stream(message.chat.id)
+    await message.reply_text("⏸ Music Di Hentikan Sementara.")
 
 
-@Client.on_message(command("resume") & other_filters)
+@Client.on_message(command(["resume", "lanjut"]) & other_filters)
 @errors
 @authorized_users_only
 async def resume(_, message: Message):
-    chat_id = get_chat_id(message.chat)
-    if (chat_id not in callsmusic.active_chats) or (
-        callsmusic.active_chats[chat_id] == "playing"
-    ):
-        await message.reply_text("❗ Tidak ada yang di jeda!")
-    else:
-        callsmusic.resume(chat_id)
-        await message.reply_text("⏸ Resumed!")
+    callsmusic.pytgcalls.resume_stream(message.chat.id)
+    await message.reply_text("▶️ Music Dilanjut.")
 
 
-@Client.on_message(command("end") & other_filters)
+@Client.on_message(command(["end", "stop"]) & other_filters)
 @errors
 @authorized_users_only
 async def stop(_, message: Message):
-    chat_id = get_chat_id(message.chat)
-    if chat_id not in callsmusic.active_chats:
-        await message.reply_text("❗ Tidak ada yang diputar!")
-    else:
-        try:
-            queues.clear(chat_id)
-        except QueueEmpty:
-            pass
+    try:
+       callsmusic.queues.clear(message.chat.id)
+    except QueueEmpty:
+       pass
 
-        await callsmusic.stop(chat_id)
-        await message.reply_text("❌ Stop pemutaran!")
+    callsmusic.pytgcalls.leave_group_call(message.chat.id)
+    await message.reply_text("❌ **Menghentikan Lagu!**")
 
 
 @Client.on_message(command("skip") & other_filters)
@@ -95,34 +65,33 @@ async def stop(_, message: Message):
 async def skip(_, message: Message):
     global que
     chat_id = get_chat_id(message.chat)
-    if chat_id not in callsmusic.active_chats:
-        await message.reply_text("❗ Tidak ada pemutaran yang di skip!")
+
+    callsmusic.queues.task_done(message.chat.id)
+    await message.reply_text("⏩ Melanjut Ke Antrian Selanjutnya.")
+    if callsmusic.queues.is_empty(message.chat.id):
+        callsmusic.pytgcalls.leave_group_call(message.chat.id)
+        await message.reply_text("Music Dihentikan, Antrian Tidak Terdeteksi")
     else:
-        queues.task_done(chat_id)
-        if queues.is_empty(chat_id):
-            await callsmusic.stop(chat_id)
-        else:
-            await callsmusic.set_stream(
-                chat_id, 
-                queues.get(chat_id)["file"]
+        callsmusic.pytgcalls.change_stream(
+                message.chat.id,
+                callsmusic.queues.get(message.chat.id)["file"]
             )
 
-    qeue = que.get(chat_id)
-    if qeue:
-        skip = qeue.pop(0)
-    if not qeue:
-        return
-    await message.reply_text(f"- Skipped **{skip[0]}**\n- Now Playing **{qeue[0][0]}**")
+        await message.reply_text(" ")
 
 
 @Client.on_message(filters.command("admincache"))
 @errors
+@authorized_users_only
 async def admincache(client, message: Message):
     set(
         message.chat.id,
-        [
+        (
             member.user
-            for member in await message.chat.get_members(filter="administrators")
-        ],
+            for member in await message.chat.get_members(
+                filter="administrators"
+            )
+        ),
     )
-    await message.reply_text("✅Admin cache refreshed!")
+
+    await message.reply_text("✅️ **Daftar admin** telah **diperbarui**")
